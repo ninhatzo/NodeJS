c5e84db9587b96e57de3939cff1393963b7af7dd
ANA CAROLINA DA SILVEIRA <ana_c_da-silveira@estudante.sc.senai.br>
2026-06-30 21:49:26 -0300

    Descarregando OT 06 dentro da OT 05

--- OT-05/ListaDeGames/index.js
+++ OT-05/ListaDeGames/index.js
@@ -3,6 +3,31 @@ const express = require("express");
 
 // Instanciando o express e carregando a biblioteca do express dentro dessa const app
 const app = express();
+app.use(express.json());
+
+app.post("/novogame", (req, res) => {
+
+    let title = req.body.title;
+    let studio = req.body.studio;
+    let price = req.body.price;
+
+    console.log(title);
+    console.log(studio);
+    console.log(price);
+
+    // Cria o objeto
+    let newGame = {
+        title,
+        studio,
+        price
+    };
+
+    // Adiciona na lista
+    games.push(newGame);
+
+    // Envia a resposta
+    res.send("OK");
+});
 
 // Lista de Games
 
